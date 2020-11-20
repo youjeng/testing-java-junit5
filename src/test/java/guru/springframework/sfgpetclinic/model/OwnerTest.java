d4f5c8a58e36fc13ae80df9f07f5116771e28b2c
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2020-11-19 21:27:57 -0500

    nested assertions with depandant assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+	@Test
+	void dependantAssertions() {
+		Owner owner = new Owner(1l, "Joe", "Buck");
+		owner.setCity("warmville");
+		owner.setTelephone("125456789");
+		
+		assertAll("propertiesTest",
+				() -> assertAll("Person Properties",
+						() -> assertEquals("Joe", owner.getFirstName(), "First Name did not match"),
+						() -> assertEquals("Duck", owner.getLastName(), "Last Name did not match")),	
+				() -> assertAll("Owner Properties",
+						() -> assertEquals("warville", owner.getCity()),
+						() -> assertEquals("125456789", owner.getTelephone())
+				));
+	}
+
+}
